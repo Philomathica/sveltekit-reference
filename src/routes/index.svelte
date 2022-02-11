8fafa0bef976a3f60853d3d4f4bc9300dc7883e6
Luuk Moret <luuk@moretconsultancy.com>
2022-02-11 14:36:25 +0100

    feat: add daiyui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,17 +14,19 @@
   export let user: User;
 </script>
 
-<h1>Welcome to SvelteKit Auth</h1>
+<div class="prose">
+  <h1>Welcome to SvelteKit Auth</h1>
+</div>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 {#if user}
   <h2>Welcome {user.name}</h2>
   <a href="/auth/logout.json" rel="external">
-    <button class="p-2 border-2">Logout</button>
+    <button class="btn btn-primary p-2 border-2">Logout</button>
   </a>
 {:else}
   <a href="/auth/login.json" rel="external">
-    <button class="p-2 border-2">Login</button>
+    <button class="btn btn-primary p-2 border-2">Login</button>
   </a>
 {/if}
